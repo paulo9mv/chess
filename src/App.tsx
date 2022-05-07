bafbf69d494d536e7c3c35c25a6726923efbcffd
Paulo Viana <paulo9mv@yahoo.com.br>
2022-05-07 19:56:38 -0400

    feat: add stockfish engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,67 @@
-import { useState } from "react";
-import chess from "chess";
+import { useEffect, useState } from "react";
+import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
+import pgn from "./pgn/example_pgn.pgn?raw";
 
-function App() {
-  return (
-    <Chessboard
-      position={"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"}
-    />
+var wasmSupported =
+  typeof WebAssembly === "object" &&
+  WebAssembly.validate(
+    Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
   );
-}
 
-export default App;
+var stockfish = new Worker(
+  !wasmSupported ? "src/stockfish.wasm.js" : "src/stockfish.js"
+);
+
+stockfish.addEventListener("message", function (e) {
+  console.log(e.data);
+
+  const a: String = e.data;
+
+  if (a.startsWith("bestmove")) {
+    console.log(a.split("bestmove"));
+  }
+});
+
+stockfish.postMessage("uci");
+
+//stockfish.postMessage("position fen r3k3/1R1ppp2/8/8/8/8/8/1Q2K3 w - - 0 1");
+stockfish.postMessage("position fen 1r2k3/8/8/8/8/8/3PPPPP/4K2R w K - 0 1");
+stockfish.postMessage("go depth 10");
+
+const PlayRandomMoveEngine = () => {
+  const [game, setGame] = useState(new Chess());
+  const [undoCount, setUndoCount] = useState(0);
+  let [initialMoves, setInitialMoves] = useState([] as any);
+
+  useEffect(() => {
+    game.load_pgn(pgn);
+    setInitialMoves(game.history());
+  }, []);
+
+  return (
+    <>
+      <button
+        disabled={undoCount === initialMoves.length}
+        onClick={() => {
+          game.undo();
+          setUndoCount(undoCount + 1);
+        }}
+      >
+        Voltar
+      </button>
+      <button
+        disabled={undoCount === 0}
+        onClick={() => {
+          game.move(initialMoves[initialMoves.length - undoCount]);
+          setUndoCount(undoCount - 1);
+        }}
+      >
+        Avançar
+      </button>
+      <Chessboard position={game.fen()} />
+    </>
+  );
+};
+
+export default PlayRandomMoveEngine;
